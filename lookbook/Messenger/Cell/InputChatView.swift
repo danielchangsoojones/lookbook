d30cce569e05478c7301bb3d4b676a779a3ee8f0
Daniel Jones <messyjones@gmail.com>
2022-02-09 02:03:43 -0800

    added the chatcellView

---
+++ lookbook/Messenger/Cell/InputChatView.swift
@@ -0,0 +1,19 @@
+//
+//  InputChatView.swift
+//  lookbook
+//
+//  Created by Daniel Jones on 2/9/22.
+//
+
+import UIKit
+
+class InputChatView: UIView {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
