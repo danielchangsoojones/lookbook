45e03b7207ac399f876afea1b3a2d4ef2e510227
dkwun <58964820+dkwun@users.noreply.github.com>
2022-02-11 01:16:12 -0800

    we can now hook up rows on settings page

--- lookbook/Account/Cell/AccountTableViewCell.swift
+++ lookbook/Account/Cell/AccountTableViewCell.swift
@@ -10,11 +10,13 @@ import Reusable
 
 class AccountTableViewCell: UITableViewCell, Reusable {
     private var titleLabel: UILabel!
+    private var rightButton: UIButton!
     
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
         self.backgroundColor = .white
         setUpTitleLabel()
+        setUpRightArrow()
     }
     
     required init?(coder: NSCoder) {
@@ -27,11 +29,29 @@ class AccountTableViewCell: UITableViewCell, Reusable {
     
     private func setUpTitleLabel() {
         titleLabel = UILabel()
-        titleLabel.text = "Log out"
+//        titleLabel.text = "Log out"
         contentView.addSubview(titleLabel)
         titleLabel.snp.makeConstraints { (make) in
             make.leading.equalToSuperview()
-            make.top.bottom.equalToSuperview().inset(5)
+            make.top.bottom.equalToSuperview().inset(10)
+        }
+    }
+    
+    func set(rowLabel: String) {
+        titleLabel.text = rowLabel
+    }
+    
+    private func setUpRightArrow() {
+        if let image = UIImage(named: "right_chevron")?.withRenderingMode(.alwaysTemplate) {
+            rightButton = UIButton()
+            rightButton.tintColor = .black
+            contentView.addSubview(rightButton)
+            rightButton.setImage(image, for: .normal)
+            rightButton.imageView?.contentMode = .scaleAspectFit
+            rightButton.snp.makeConstraints { make in
+                make.centerY.equalTo(titleLabel)
+                make.trailing.equalToSuperview().inset(10)
+            }
         }
     }
 }
