c50817d879bd3211660812b2b0a4ec298cb3a192
Tyler Flowers <flowersjptyler@gmail.com>
2022-02-11 23:09:17 -0800

    VerificationCheck

---
+++ lookbook/Master Chat Room/Cell/BroadcastViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  BroadcastViewCell.swift
+//  lookbook
+//
+//  Created by Tyler Flowers on 2/11/22.
+//
+
+import Foundation
+
+class BroadcastViewCell: MasterChatRoomTableViewCell {
+    
+}
